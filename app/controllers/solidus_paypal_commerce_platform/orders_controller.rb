emboldagency/solidus_paypal_commerce_platform

commit 8420d9ec498e3e766bd940f09d9a738b61abd4a7

:alien: Fix deprecations

app/controllers/solidus_paypal_commerce_platform/orders_controller.rb
@@ -19,7 +19,7 @@ module SolidusPaypalCommercePlatform
                 cookies.signed[:guest_token] = @order.guest_token
                 render(json: @order, status: :ok)
             else
-                render(json: @order.errors.full_messages, status: :unprocessable_entity)
+                render(json: @order.errors.full_messages, status: :unprocessable_content)
             end
         end
 
@@ -29,11 +29,11 @@ module SolidusPaypalCommercePlatform
             paypal_address = SolidusPaypalCommercePlatform::PaypalAddress.new(@order)
 
             if paypal_address.update(paypal_address_params).valid?
-                @order.ensure_updated_shipments
+                @order.check_shipments_and_restart_checkout
                 @order.contents.advance
                 render(json: {}, status: :ok)
             else
-                render(json: paypal_address.errors.full_messages, status: :unprocessable_entity)
+                render(json: paypal_address.errors.full_messages, status: :unprocessable_content)
             end
         end
 

app/models/solidus_paypal_commerce_platform/paypal_address.rb
@@ -13,7 +13,7 @@ module SolidusPaypalCommercePlatform
 
             return unless @order.ship_address.valid?
 
-            @order.ensure_updated_shipments
+            @order.check_shipments_and_restart_checkout
             @order.email = "info@solidus.io" unless @order.email
             @order.contents.advance
         end

app/controllers/solidus_paypal_commerce_platform/shipping_rates_controller.rb
@@ -18,7 +18,7 @@ module SolidusPaypalCommercePlatform
             if @errors.none?
                 render(json: @paypal_order, status: :ok)
             else
-                render(json: @errors.full_messages, status: :unprocessable_entity)
+                render(json: @errors.full_messages, status: :unprocessable_content)
             end
         end
 

app/controllers/solidus_paypal_commerce_platform/wizard_controller.rb
@@ -19,7 +19,7 @@ module SolidusPaypalCommercePlatform
                     notice: "The PayPal Commerce Platform payment method has been successfully created",
                 )
             else
-                render(json: @payment_method.errors, status: :unprocessable_entity)
+                render(json: @payment_method.errors, status: :unprocessable_content)
             end
         end
 

spec/requests/solidus_paypal_commerce_platform/shipping_rates_controller_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe(SolidusPaypalCommercePlatform::ShippingRatesController, type: :re
             let(:new_state) { nil }
 
             it "returns a list of errors" do
-                expect(response).to(have_http_status(:unprocessable_entity)) # unprocessable_entity
+                expect(response).to(have_http_status(:unprocessable_content)) # unprocessable_entity
                 expect(response.body).to(include("State can't be blank"))
             end
         end

commit 22426e9e3ebbc816c6d9c7f609b6c13947a66083

:bug: Fix loop caused by 8420d9e deprecated ensure_updated_shipments replacement

app/controllers/solidus_paypal_commerce_platform/orders_controller.rb
@@ -29,7 +29,7 @@ module SolidusPaypalCommercePlatform
             paypal_address = SolidusPaypalCommercePlatform::PaypalAddress.new(@order)
 
             if paypal_address.update(paypal_address_params).valid?
-                @order.check_shipments_and_restart_checkout
+                @order.update_shipments if @order.respond_to?(:update_shipments)
                 @order.contents.advance
                 render(json: {}, status: :ok)
             else

app/models/solidus_paypal_commerce_platform/paypal_address.rb
@@ -13,7 +13,7 @@ module SolidusPaypalCommercePlatform
 
             return unless @order.ship_address.valid?
 
-            @order.check_shipments_and_restart_checkout
+            @order.update_shipments if @order.respond_to?(:update_shipments)
             @order.email = "info@solidus.io" unless @order.email
             @order.contents.advance
         end